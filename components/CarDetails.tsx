015d6bb7d20b83f30f2005c0630269c2ba2cab4a
nickcookies <nikos_outlook.com>
2023-09-22 17:42:56 +0300

    feat:New car details added/ search params updated

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,6 +4,7 @@ import { CarProps } from "@/types";
 import React, { Fragment } from "react";
 import Image from "next/image";
 import { Dialog, Transition } from "@headlessui/react";
+import { generateCarImageUrl } from "@/utils";
 
 interface CarDatailsProps {
   isOpen: boolean;
@@ -55,7 +56,7 @@ const CarDetails = ({ isOpen, car, closeModal }: CarDatailsProps) => {
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src="/hero.png"
+                        src={generateCarImageUrl(car, "angle")}
                         alt="car model"
                         fill
                         priority
@@ -65,7 +66,7 @@ const CarDetails = ({ isOpen, car, closeModal }: CarDatailsProps) => {
                     <div className="flex gap-3">
                       <div className="flex-1 relative w-full bg-primary-blue-100 rounded-lg h-24">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "29")}
                           alt="car model"
                           fill
                           priority
@@ -74,7 +75,7 @@ const CarDetails = ({ isOpen, car, closeModal }: CarDatailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full bg-primary-blue-100 rounded-lg h-24">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "33")}
                           alt="car model"
                           fill
                           priority
@@ -83,7 +84,7 @@ const CarDetails = ({ isOpen, car, closeModal }: CarDatailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full bg-primary-blue-100 rounded-lg h-24">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "13")}
                           alt="car model"
                           fill
                           priority
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "9b90466c47msh4ee31c79bf313efp10c292jsn31a8736c8ad4",
@@ -5,7 +7,7 @@ export async function fetchCars() {
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
     { headers: headers }
   );
 
@@ -26,4 +28,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoom", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
