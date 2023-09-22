0cc2d02462533dbca6db47f706abf860c35c6620
nickcookies <nikos_outlook.com>
2023-09-22 12:56:50 +0300

    feat:Project Structure

---
+++ components/index.ts
@@ -0,0 +1,9 @@
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+export { CustomButton };
+export { Footer };
+export { Hero };
+export { Navbar };
