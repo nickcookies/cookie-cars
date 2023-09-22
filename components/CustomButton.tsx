a30b3786d6f399e409f6c1a2c942922530669912
nickcookies <nikos_outlook.com>
2023-09-22 13:40:58 +0300

    feat:Finalise layout

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,12 +7,13 @@ import { CustomButtonProps } from "@/types";
 const CustomButton = ({
   title,
   containerStyles,
+  btnType,
   handleClick,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
